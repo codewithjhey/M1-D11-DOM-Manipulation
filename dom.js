b846ea2befc2d40d75b8a6eba0afa285dcd3a67f
codewithjhey <114162135+codewithjhey@users.noreply.github.com>
2022-10-10 15:55:01 +0100

    Homework

---
+++ dom.js
@@ -0,0 +1,18 @@
+/* …from 7 to 12, should be written in JavaScript:
+
+EX7: Write a function to change the h1 text to something else
+
+*/
+
+document.getElementById("main-title")
+console.dir(h1)
+
+/*EX8: Write a function to change the page background color
+
+EX9: Write a function to change the footer address with a fake one
+
+EX10: Write a function to add a CSS class to every Amazon link
+
+EX11: Write a function to toggle a CSS class for all the images in the table; that class should set the visibility of the image
+
+EX12: Write a function to color the price of the products in a different one every time it’s invoked */
