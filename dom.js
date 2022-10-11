23bfa1d9192ac3ece3b7ffd6e2fbff24cf61baf2
codewithjhey <114162135+codewithjhey@users.noreply.github.com>
2022-10-11 02:53:53 +0100

    final exercise completed with styling

--- dom.js
+++ dom.js
@@ -4,28 +4,53 @@ EX7: Write a function to change the h1 text to something else
 
 */
 
-function changeH1Text() {
+const changeH1Text = function () {
   document.quarySelector("h1").innerText = "Luxuxy by Jay"
 }
+
+changeH1Text()
 
 /*EX8: Write a function to change the page background color
  */
 
-function changeBackgroundColor() {
-  document.querySelector("body").style.changeBackgroundColor = lightgreen
+const changeBackgroundColor = function () {
+  document.querySelector("body").style.backgroundColor = "lightblue"
 }
 
 changeBackgroundColor()
 
 //EX9: Write a function to change the footer address with a fake one
 
-function changeFooterAddress() {
-  document.querySelector("#address").InnerHTML = " Lekki, Lagos, Nigeria"
+const changeFooterAddress = function () {
+  document.querySelector("footer > div:nth-child(2)").InnerHTML =
+    "Lekki, Lagos, Nigeria This is a fake address Lol"
 }
 
-/*
-EX10: Write a function to add a CSS class to every Amazon link
+changeFooterAddress()
 
-EX11: Write a function to toggle a CSS class for all the images in the table; that class should set the visibility of the image
+//EX10: Write a function to add a CSS class to every Amazon link
 
-EX12: Write a function to color the price of the products in a different one every time it’s invoked */
+const addClassToLinks = function () {
+  for (let node of document.querySelectorAll("span")) {
+    node.classList.add("big-font")
+  }
+}
+
+addClassToLinks()
+
+//EX11: Write a function to toggle a CSS class for all the images in the table; that class should set the visibility of the image
+const toggleClassOfImg = function () {
+  for (let image of document.querySelectorAll("img")) {
+    image.classList.toggle("hidden")
+  }
+}
+toggleClassOfImg()
+
+//EX12: Write a function to color the price of the products in a different one every time it’s invoked */
+
+const setPriceColor = function getPriceColor() {
+  for (let price of document.querySelectorAll("tr > td:nth-child(4)")) {
+    price.style.color = color
+  }
+}
+setPriceColor()
